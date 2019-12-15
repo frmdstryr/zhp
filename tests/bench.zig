5e9558451eac0b21ab302e515d6b5606366583e7
frmdstryr <frmdstryr@protonmail.com>
2019-12-14 19:34:51 -0500

    Update test

--- tests/bench.zig
+++ tests/bench.zig
@@ -1,13 +1,12 @@
 const std = @import("std");
 
 const server_cmd = [_][]const u8{
-    "timeout", "-s", "SIGINT", "60s",
-    "bash", "-c", "zig build -Drelease-fast=true run",
+    "timeout", "-s", "SIGINT", "15s", "./zig-cache/bin/zhttpd",
 };
 
 const wrk_cmd = [_][]const u8{
-    "docker", "run", "--rm", "williamyeh/wrk",
-    "-t2", "-c100", "-d30s", "--latency", "http://127.0.0.1:9000/"
+    "docker", "run", "--rm", "--net", "host", "williamyeh/wrk",
+    "-t2", "-c10", "-d10s", "--latency", "http://127.0.0.1:9000/"
 };
 
 pub fn main() anyerror!void {
@@ -16,8 +15,8 @@ pub fn main() anyerror!void {
     defer server_process.deinit();
     try server_process.spawn();
 
-    // Wait for it to build
-    std.time.sleep(20*std.time.ns_per_s);
+    // Wait for it to start
+    std.time.sleep(1*std.time.ns_per_s);
 
     var wrk_process = try std.ChildProcess.init(wrk_cmd[0..], allocator);
     defer wrk_process.deinit();
